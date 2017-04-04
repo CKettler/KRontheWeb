20c275192c96b2c75ac08d4947fda4f00e38c388
Panagiotis Eustratiadis <ep.pan.dit@gmail.com>
2017-04-04 16:33:41 +0200

    accepting file paths as parameters

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+
 def csv_to_dataframe(file_path, key):
     if key == 'data':
-        dataframe =  pd.read_csv(file_path, delimiter=';')
+        dataframe = pd.read_csv(file_path, delimiter=';')
     else:
         dataframe = pd.read_csv(file_path, delimiter=',')
     dataframe.columns = [x.lower() for x in dataframe.columns]
     return(dataframe)
+
 
 def combine_dataframes(df_data, df_metadata):
     df_all = pd.merge(df_data, df_metadata, on='variabele', how='inner')
--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
-import data_preparation as prep
-import pandas as pd
+import sys
 
-# file_paths to the data and metadata
-file_path_data = "../data/areas_data.csv"
-file_path_meta_data = "../data/areas_metadata.csv"
+import data_preparation as prep
+
+
+def usage():
+    print('Not all parameters specified. Correct usage:')
+    print('python main.py path/to/dataset path/to/metadata path/to/output')
+
+
+# file_paths to the data, metadata and output
+file_path_data = sys.argv[1]
+file_path_metadata = sys.argv[2]
+file_path_output = sys.argv[3]
+
+if not (file_path_data and file_path_metadata and file_path_output):
+    usage()
+    exit(1)
 
 # transform the csv files to two dataframes
 df_data = prep.csv_to_dataframe(file_path=file_path_data, key='data')
-df_metadata = prep.csv_to_dataframe(file_path=file_path_meta_data, key='metadata')
+df_metadata = prep.csv_to_dataframe(
+    file_path=file_path_metadata, key='metadata')
+
 print("[files transformed to dataframes]")
 
 # combine these dataframes in one dataframe and save this to a csv
 df_all_data = prep.combine_dataframes(df_data, df_metadata)
 print("[saving data to csv...]")
-df_all_data.to_csv("../data/areas_all_data.csv")
+df_all_data.to_csv(file_path_output)
 print("[saved!]")
-
