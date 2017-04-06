adbcbcb4f3fdc5887ea335d505764e613dea6eef
Celeste Kettler <celestekettler@gmail.com>
2017-04-06 10:39:48 +0200

    fully working

--- src/combine_triples.py
+++ src/combine_triples.py
@@ -22,10 +22,12 @@ except IndexError:
 # df_data = prep.dataset_to_triples(file_path=file_path_data, year=2017)
 df_data = prep.csv_to_dataframe(
     file_path=file_path_data, key='data')
+df_data = df_data.loc[df_data['jaar'] == 2017]
 df_metadata = prep.csv_to_dataframe(
     file_path=file_path_metadata, key='metadata')
 df_metadata_2 = prep.csv_to_dataframe(
-    file_path=file_path_metadata_2, key='metadata2')[['gebiedcode15', 'gebiednaam']]
+    file_path=file_path_metadata_2, key='metadata2')
+df_metadata_2 = df_metadata_2.loc[df_metadata_2['jaar'] == 2017][['gebiedcode15', 'gebiednaam']]
 
 print("[files transformed to dataframes]")
 
