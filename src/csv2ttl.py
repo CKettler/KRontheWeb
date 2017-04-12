9fcfa6125d0e23ea6885560db45943fc1959abd2
Panagiotis Eustratiadis <ep.pan.dit@gmail.com>
2017-04-12 15:25:35 +0200

    new version of triples script including restaurants

--- src/csv2ttl.py
+++ src/csv2ttl.py
@@ -3,17 +3,9 @@ import csv
 import sys
 
 from iribaker import to_iri
-from rdflib import Dataset, URIRef, Literal, Namespace, RDF, RDFS, OWL, XSD
-
-dataset_path = sys.argv[1]
-
-VARIABELE_IDX = 0
-GEBIEDCODE_IDX = 1
-WAARDE_IDX = 2
-LABEL_IDX = 3
-DEFINITIE_IDX = 4
-GEBIEDNAAM_IDX = 5
-SDNAAM_IDX = 6
+from rdflib import (
+    Dataset, URIRef, Literal, Namespace,
+    RDF as rdf, RDFS as rdfs, OWL as owl, XSD)
 
 
 class UTF8Recoder:
@@ -48,88 +40,169 @@ class UnicodeReader:
         return self
 
 
-data = 'http://data.krw.d2s.labs.vu.nl/group1/resource/'
-DATA = Namespace(data)
+data = Namespace('http://data.krw.d2s.labs.vu.nl/group1/resource/')
 
-# DBPEDIA namespace
-dbr = 'http://dbpedia.org/resource/'
-DBR = Namespace(dbr)
-dbo = 'http://dbpedia.org/ontology/'
-DBO = Namespace(dbo)
+# other namespaces
+dbr = Namespace('http://dbpedia.org/resource/')
+dbo = Namespace('http://dbpedia.org/ontology/')
+org = Namespace('https://www.w3.org/ns/org#')
+geo = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+review = Namespace('http:/www.purl.org/stuff/rev#')
 
 # The URI for our graph
 graph_uri = URIRef('http://data.krw.d2s.labs.vu.nl/group1/resource/graph')
 
 # We initialize a dataset, and bind our namespaces
 dataset = Dataset()
-dataset.bind('g01data', DATA)
-dataset.bind('dbr', DBR)
-dataset.bind('dbo', DBO)
+dataset.bind('g01data', data)
+dataset.bind('dbr', dbr)
+dataset.bind('dbo', dbo)
+dataset.bind('org', org)
+dataset.bind('geo', geo)
 
-# We then get a new graph object with our URI from the dataset.
-graph = dataset.graph(graph_uri)
 
-visited = []
-with open(dataset_path, "r") as csvfile:
-    csv_contents = UnicodeReader(csvfile)
-    header = csv_contents.next()
-    # Let's iterate over the dictionary, and create some triples
-    # Let's pretend we know exactly what the 'schema' of our CSV file is
-    count = 0
-    for row in csv_contents:
-        # Progress
-        count += 1
-        if count % 10000 == 0:
-            print "Progress: " + str(count)
+def build_graph_for_areas(graph, areas_csv):
+    VARIABELE_IDX = 0
+    GEBIEDCODE_IDX = 1
+    WAARDE_IDX = 2
+    LABEL_IDX = 3
+    DEFINITIE_IDX = 4
+    GEBIEDNAAM_IDX = 5
+    SDNAAM_IDX = 6
 
-        variable = row[VARIABELE_IDX]
-        vb_entity = URIRef(to_iri(data + variable))
-        if variable not in visited:
-            visited.append(variable)
-            def_literal = Literal(row[DEFINITIE_IDX], lang='nl')
-            label_literal = Literal(row[LABEL_IDX], lang='nl')
-            graph.add((vb_entity, RDFS.isDefinedBy, def_literal))
-            graph.add((vb_entity, RDFS.label, label_literal))
+    visited = []
+    with open(areas_csv, "r") as csvfile:
+        csv_contents = UnicodeReader(csvfile)
+        csv_contents.next()
+        count = 0
+        for row in csv_contents:
+            # Progress
+            count += 1
+            if count % 10000 == 0:
+                print "Progress: " + str(count)
 
-        try:
+            variable = row[VARIABELE_IDX]
+            vb_entity = URIRef(to_iri(data + variable))
+            if variable not in visited:
+                visited.append(variable)
+                def_literal = Literal(row[DEFINITIE_IDX], lang='nl')
+                label_literal = Literal(row[LABEL_IDX], lang='nl')
+                graph.add((vb_entity, rdf.type, rdf.Property))
+                graph.add((vb_entity, rdfs.isDefinedBy, def_literal))
+                graph.add((vb_entity, rdfs.label, label_literal))
+
             try:
-                gb = int(row[GEBIEDNAAM_IDX])
-                continue
+                try:
+                    int(row[GEBIEDNAAM_IDX])
+                    continue
+                except ValueError:
+                    pass
+
+                gb_code = row[GEBIEDCODE_IDX]
+                gb_name = row[GEBIEDNAAM_IDX]
+                sd_name = row[SDNAAM_IDX]
+
+                area = URIRef(to_iri(data + gb_name))
+                value = Literal(
+                    float(row[WAARDE_IDX]), datatype=XSD['decimal'])
+
+                graph.add((area, rdf.type, rdfs.Class))
+                graph.add((area, vb_entity, value))
+                graph.add((area, data['areaCode'], Literal(
+                    row[GEBIEDCODE_IDX], datatype=XSD['string'])))
+                if gb_code == 'STAD':
+                    graph.add((area, rdf.type, dbo.City))
+                    graph.add((area, rdf.type, org.Site))
+                    graph.add((area, owl.sameAs, dbr.Amsterdam))
+                else:
+                    graph.add((area, rdf.type, dbo.Area))
+                    if gb_code + " " + gb_name == sd_name:
+                        subarea_val = Literal("Amsterdam", lang='nl')
+                        subarea_uri = URIRef(to_iri(data + 'Amsterdam'))
+                    else:
+                        try:
+                            literal = "Amsterdam " + sd_name.split()[1]
+                            subarea_val = Literal(literal, lang='nl')
+                            subarea_uri = URIRef(to_iri(data + literal))
+                        except IndexError:
+                            continue
+                    graph.add((area, dbo.isPartOf, subarea_uri))
+                    graph.add((subarea_uri, rdfs.label, subarea_val))
+
             except ValueError:
                 pass
+    return graph
 
-            gb_code = row[GEBIEDCODE_IDX]
+
+def build_graph_for_restaurants(graph, restaurants_csv):
+    GEBIEDCODE_IDX = 0
+    GEBIEDNAAM_IDX = 1
+    LONGITUDE_IDX = 2
+    LATITUDE_IDX = 3
+    BUSINESS_ID_IDX = 4
+    BUSINESS_NAME_IDX = 5
+    REVIEW_COUNT_IDX = 6
+    RATING_IDX = 7
+    PRICE_IDX = 8
+
+    with open(restaurants_csv, "r") as csvfile:
+        csv_contents = UnicodeReader(csvfile)
+        csv_contents.next()
+        count = 0
+        for row in csv_contents:
+            # Progress
+            count += 1
+            if count % 10000 == 0:
+                print "Progress: " + str(count)
+
             gb_name = row[GEBIEDNAAM_IDX]
-            sd_name = row[SDNAAM_IDX]
-
+            restaurant_name = row[BUSINESS_NAME_IDX]
+            longitude = row[LONGITUDE_IDX] \
+                if row[LONGITUDE_IDX] != 'None' else None
+            latitude = row[LATITUDE_IDX] \
+                if row[LONGITUDE_IDX] != 'None' else None
             area = URIRef(to_iri(data + gb_name))
-            value = Literal(float(row[WAARDE_IDX]), datatype=XSD['decimal'])
+            restaurant = URIRef(to_iri(data + restaurant_name))
 
-            # All set... we are now going to add the triples to our graph
-            graph.add((area, vb_entity, value))
+            graph.add((area, org.siteOf, restaurant))
+            graph.add((area, data['locationOf'], restaurant))
 
-            if gb_code == 'STAD':
-                graph.add((area, RDF.type, DBO.City))
-                graph.add((area, OWL.sameAs, DBR.Amsterdam))
-            else:
-                graph.add((area, RDF.type, DBO.Area))
-                if gb_code + " " + gb_name == sd_name:
-                    subarea_val = Literal("Amsterdam", lang='nl')
-                    subarea_uri = URIRef(to_iri(data + 'Amsterdam'))
-                else:
-                    try:
-                        literal = "Amsterdam " + sd_name.split()[1]
-                        subarea_val = Literal(literal, lang='nl')
-                        subarea_uri = URIRef(to_iri(data + literal))
-                    except IndexError:
-                        continue
-                graph.add((area, DBO.isPartOf, subarea_uri))
-                graph.add((subarea_uri, RDFS.label, subarea_val))
+            graph.add((restaurant, rdf.type, rdfs.Class))
+            graph.add((restaurant, rdf.type, dbr.Business))
+            graph.add((restaurant, rdf.type, dbo.Restaurant))
+            graph.add((restaurant, rdf.type, org.Organization))
+            if longitude and latitude:
+                graph.add((restaurant, geo.lat, Literal(
+                    float(longitude), datatype=XSD['decimal'])))
+                graph.add((restaurant, geo.logn, Literal(
+                    float(latitude), datatype=XSD['decimal'])))
+            graph.add((restaurant, org.hasSite, area))
+            graph.add((area, data['locatedIn'], restaurant))
+            graph.add((restaurant, review.minRating, Literal(
+                1, datatype=XSD['integer'])))
+            graph.add((restaurant, review.maxRating, Literal(
+                5, datatype=XSD['integer'])))
+            graph.add((restaurant, review.rating, Literal(
+                float(row[RATING_IDX]), datatype=XSD['decimal'])))
+            graph.add((restaurant, review.totalVotes, Literal(
+                int(row[REVIEW_COUNT_IDX]), datatype=XSD['integer'])))
+            graph.add((restaurant, data['hasPrice'], Literal(
+                row[PRICE_IDX], datatype=XSD['string'])))
+            graph.add((area, data['businessID'], Literal(
+                row[BUSINESS_ID_IDX], datatype=XSD['string'])))
 
-        except ValueError:
-            pass
+    return graph
 
-    print "Saving file..."
-    with open('out.ttl', 'w') as f:
+
+if __name__ == '__main__':
+    areas_csv = sys.argv[1]
+    restaurants_csv = sys.argv[2]
+    graph = dataset.graph(graph_uri)
+
+    graph = build_graph_for_areas(graph, areas_csv)
+    graph = build_graph_for_restaurants(graph, restaurants_csv)
+
+    print("Saving graph in turtle format...")
+    with open('graph.ttl', 'w') as f:
         graph.serialize(f, format='turtle')
-    print "Done!"
+    print("Done!")
